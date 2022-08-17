953f9e9ccac5b711e0a9f8737b5887881aa89a1f
clarkee <69399383+ClarkeeCity@users.noreply.github.com>
2022-08-17 07:29:13 -0400

    fixed playpause component rerendering issue, toggle now works as intended

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,89 +1,22 @@
-import { useEffect, SetStateAction, useState } from 'react';
+import { useState } from 'react';
 import './components/main.scss';
-import { IpcRendererEvent } from 'electron';
-import Playlist from './components/Playlist/Playlist';
-import Experiences from './components/Experiences/Experiences';
-import Sidebar from './components/Sidebar/Sidebar';
-import BottomBar from './components/BottomBar/BottomBar';
 import InterfaceContainer from './components/Containers/InterfaceContainer/InterfaceContainer';
-import InterfaceMain from './components/Containers/InterfaceMain/InterfaceMain';
 import AppContainer from './components/Containers/AppContainer/AppContainer';
-import PlaylistContainer from './components/Containers/PlaylistContainer/PlaylistContainer';
 // import TitleBar from './components/TitleBar/TitleBar';
 import Player from '../Player';
-import Row from './components/Playlist/Row';
-import MediaCtrl from './components/BottomBar/MediaCtrl/MediaCtrl';
-import {
-  Play,
-  Pause,
-  PlayPause,
-  SkipBackward,
-  SkipForward,
-} from './components/BottomBar/PlaybackButtons';
 
 const player = new Player();
 
 export default function App() {
-  // List of files to render on playlist.
-  const [data, setData] = useState<string[]>([]);
-  player.playlist = data;
-  // Fetch files from Ethereal.library if it exists onmount.
-  useEffect(() => {
-    // eslint-disable-next-line promise/catch-or-return, promise/always-return
-    window.electron.initPlaylist().then((resolve) => {
-      setData(resolve);
-    });
-  }, []);
-  window.electron.updatePlaylist(
-    (_event: IpcRendererEvent, value: SetStateAction<string[]>) => {
-      setData(value);
-    }
-  );
-
   const [stream, setStream] = useState<string[]>(['']);
-  const [playToggle, setPlayToggle] = useState<boolean>(false);
   // anytime the stream is updated, set new howl to play.
   player.setHowl(stream);
-
-  const songs = data
-    ? data.map((song) => (
-        <Row
-          key={song}
-          fileData={song}
-          setStream={setStream}
-          setToggle={setPlayToggle}
-        />
-      ))
-    : [];
 
   return (
     <AppContainer>
       {/* If we want to have a custom titlebar */}
       {/* <TitleBar /> */}
-      <InterfaceContainer>
-        <Experiences />
-        <InterfaceMain>
-          <PlaylistContainer>
-            <Sidebar />
-            {/* <Playlist playlistData={data} setStream={setStream} /> */}
-            <Playlist>{songs}</Playlist>
-            <Sidebar />
-          </PlaylistContainer>
-          <BottomBar>
-            <MediaCtrl>
-              <SkipBackward />
-              <PlayPause
-                player={player}
-                toggle={playToggle}
-                setToggle={setPlayToggle}
-              />
-              {/* <Play player={player} />
-              <Pause player={player} /> */}
-              <SkipForward />
-            </MediaCtrl>
-          </BottomBar>
-        </InterfaceMain>
-      </InterfaceContainer>
+      <InterfaceContainer player={player} setStream={setStream} />
     </AppContainer>
   );
 }
--- src/renderer/components/Containers/InterfaceContainer/InterfaceContainer.tsx
+++ src/renderer/components/Containers/InterfaceContainer/InterfaceContainer.tsx
@@ -1,9 +1,79 @@
+import { IpcRendererEvent } from 'electron';
+import Player from 'Player';
+import { SetStateAction, useEffect, useState } from 'react';
+import BottomBar from 'renderer/components/BottomBar/BottomBar';
+import MediaCtrl from 'renderer/components/BottomBar/MediaCtrl/MediaCtrl';
+import {
+  PlayPause,
+  SkipBackward,
+  SkipForward,
+} from 'renderer/components/BottomBar/PlaybackButtons';
+import Experiences from 'renderer/components/Experiences/Experiences';
+import Playlist from 'renderer/components/Playlist/Playlist';
+import Row from 'renderer/components/Playlist/Row';
+import Sidebar from 'renderer/components/Sidebar/Sidebar';
+import InterfaceMain from '../InterfaceMain/InterfaceMain';
+import PlaylistContainer from '../PlaylistContainer/PlaylistContainer';
 import './interfacecontainer.scss';
 
-type Props = {
-  children: JSX.Element | JSX.Element[];
-};
+interface InterfaceContainerProps {
+  player: Player;
+  setStream: React.Dispatch<SetStateAction<string[]>>;
+}
 
-export default function InterfaceContainer({ children }: Props) {
-  return <div id="interface-container">{children}</div>;
+export default function InterfaceContainer({
+  setStream,
+  player,
+}: InterfaceContainerProps) {
+  // List of files to render on playlist.
+  const [data, setData] = useState<string[]>([]);
+  player.playlist = data;
+  // Fetch files from Ethereal.library if it exists onmount.
+  useEffect(() => {
+    // eslint-disable-next-line promise/catch-or-return, promise/always-return
+    window.electron.initPlaylist().then((resolve) => {
+      setData(resolve);
+    });
+  }, []);
+  window.electron.updatePlaylist(
+    (_event: IpcRendererEvent, value: SetStateAction<string[]>) => {
+      setData(value);
+    }
+  );
+
+  const [playToggle, setPlayToggle] = useState<boolean>(false);
+
+  const songs = data
+    ? data.map((song) => (
+        <Row
+          key={song}
+          fileData={song}
+          setStream={setStream}
+          setToggle={setPlayToggle}
+        />
+      ))
+    : [];
+  return (
+    <div id="interface-container">
+      <Experiences />
+      <InterfaceMain>
+        <PlaylistContainer>
+          <Sidebar />
+          <Playlist>{songs}</Playlist>
+          <Sidebar />
+        </PlaylistContainer>
+        <BottomBar>
+          <MediaCtrl>
+            <SkipBackward />
+            <PlayPause
+              player={player}
+              toggle={playToggle}
+              setToggle={setPlayToggle}
+            />
+            <SkipForward />
+          </MediaCtrl>
+        </BottomBar>
+      </InterfaceMain>
+    </div>
+  );
 }
